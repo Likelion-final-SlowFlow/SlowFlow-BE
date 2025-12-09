1aa574f84e601e4367c273784df18026b6bb6acd
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-12-09 23:57:35 +0900

    Initial project setup

---
+++ src/test/java/com/slowflow/SlowFlow_backend/SlowFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slowflow.SlowFlow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlowFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
