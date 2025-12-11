14955c6d55e091ee3312ff558cc2dc8ffbe65438
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-12-12 00:59:35 +0900

    chore: 파일 삭제

--- src/main/java/com/slowflow/SlowFlow_backend/global/exception/GlobalExceptionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.slowflow.SlowFlow_backend.global.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler {
-
-}
