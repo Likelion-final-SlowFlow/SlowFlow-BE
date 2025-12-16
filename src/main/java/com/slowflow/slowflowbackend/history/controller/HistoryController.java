466fe8ca779a5529201b88b563bfdbe86045c674
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-12-17 02:01:53 +0900

    feat: 컨트롤러 생성

---
+++ src/main/java/com/slowflow/slowflowbackend/history/controller/HistoryController.java
@@ -0,0 +1,35 @@
+package com.slowflow.slowflowbackend.history.controller;
+
+import com.slowflow.slowflowbackend.global.response.ApiResponse;
+import com.slowflow.slowflowbackend.history.dto.WeeklyHistoryResponse;
+import com.slowflow.slowflowbackend.history.service.HistoryService;
+import com.slowflow.slowflowbackend.member.model.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/history")
+public class HistoryController {
+
+    private final HistoryService historyService;
+
+    // 주간 히스토리 조회
+    @GetMapping("/weekly")
+    public ResponseEntity<ApiResponse<WeeklyHistoryResponse>> getWeekly(
+            Authentication authentication,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            LocalDate baseDate
+    ) {
+        Member member = (Member) authentication.getPrincipal();
+
+        WeeklyHistoryResponse result = historyService.getWeeklyHistory(member, baseDate);
+        return ResponseEntity.ok(ApiResponse.ok("주간 히스토리 조회 성공", result));
+    }
+}
